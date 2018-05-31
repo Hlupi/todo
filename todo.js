f95c3fc477e0df4e2b152abd2ef89d881cf70010
Hlupi <yu.ashikhmina@gmail.com>
2018-05-31 14:56:59 +0200

    push data into new lis

--- todo.js
+++ todo.js
@@ -1,11 +1,27 @@
-var todoList = [
+var todoData = [
   'Work hard',
   'Sleep hard',
   'Treat yourself',
   'Do the victory dance'];
 
-  console.log(todoList);
+  console.log(todoData);
 
-  todoList.forEach(function(todo) {
-    console.log(todo);
-  });
+  var newUl = document.createElement("ul");
+
+  for (var i = 0; i < todoData.length; i++) {
+    // console.log(i);
+    // console.log(todoData[i]);
+
+    var newListItem = document.createElement("li");
+    // console.log(newListItem, "this is newListItem");
+
+    var newContent = document.createTextNode(todoData[i]);
+    console.log(newContent, "this is new content");
+    newListItem.appendChild(newContent);
+  }
+
+  // todoData.forEach(function(todo) {
+  //   var listItem = document.createElement("li");
+  //   var todoText = document.createTextNode(todo);
+  //   listItem.appendChild(todoText);
+  // });
