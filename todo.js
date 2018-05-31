b098d05942cf61480d29feae1d14ac8ce2d034e9
Hlupi <yu.ashikhmina@gmail.com>
2018-05-31 14:28:50 +0200

    link js file, create array var, iterate over the array

---
+++ todo.js
@@ -0,0 +1,11 @@
+var todoList = [
+  'Work hard',
+  'Sleep hard',
+  'Treat yourself',
+  'Do the victory dance'];
+
+  console.log(todoList);
+
+  todoList.forEach(function(todo) {
+    console.log(todo);
+  });
